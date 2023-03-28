2852701124491b48514f0f7fd95cad4e2047736e
Tiago de Andrade Freire <freire.tiago@gmail.com>
2023-03-27 23:47:43 -0300

    fix: restore

--- react/components/B2BRepresentativeArea/B2BRepresentativeArea.tsx
+++ react/components/B2BRepresentativeArea/B2BRepresentativeArea.tsx
@@ -6,7 +6,7 @@ import { Progress, Spinner } from 'vtex.styleguide'
 
 import { B2BContext, B2BContextProps } from '../../context/B2BContext'
 import {
-  getGoal,
+  // getGoal,
   getMonthlyOrders,
   getPercentReachedValue,
   getPercentReachedValueFormatted,
@@ -30,7 +30,7 @@ function B2BRepresentativeArea(
   const [loading, setLoading] = useState(true)
   const { data, setData } = React.useContext(B2BContext)
   const handles = useCssHandles(CSS_HANDLES)
-  const [goal, setGoal] = useState(0)
+  // const [goal, setGoal] = useState(0)
   const [monthlyOrders, setMonthlyOrders] = useState<{
     totalValue: number
     monthlyOrdersDistinctClientAmount: number
@@ -46,16 +46,16 @@ function B2BRepresentativeArea(
     getUser().then(recoveredUser => {
       setUser(recoveredUser)
 
+      // recoveredUser.organization &&
+      //   getGoal(recoveredUser.organization).then(goal => {
+      //     setGoal(goal)
       recoveredUser.organization &&
-        getGoal(recoveredUser.organization).then(goal => {
-          setGoal(goal)
-          recoveredUser.organization &&
-            recoveredUser.costCenter &&
-            getMonthlyOrders().then(orders => {
-              setMonthlyOrders(orders)
-              setLoading(false)
-            })
+        recoveredUser.costCenter &&
+        getMonthlyOrders().then(orders => {
+          setMonthlyOrders(orders)
+          setLoading(false)
         })
+      // })
     })
   }, [])
 
@@ -64,17 +64,17 @@ function B2BRepresentativeArea(
       ...prevData,
       individualGoal: {
         description: prevData.individualGoal.description,
-        value: goal /* 
-          ? goal
-          : user?.organization === '47da0c2b-a4a5-11ec-835d-02bbf463c079'
-          ? 40000
-          : user?.organization === 'df6965b9-a499-11ec-835d-0aa8762320bd'
-          ? 35000
-          : user?.organization === '4b3635cf-b937-11ed-83ab-02032078fba7'
-          ? 30000
-          : user?.organization === '0d3ea49a-c1e6-11ed-83ab-12e19e79322b'
-          ? 25000
-          : props.individualGoal */,
+        // goal
+        value:
+          user?.organization === '47da0c2b-a4a5-11ec-835d-02bbf463c079'
+            ? 40000
+            : user?.organization === 'df6965b9-a499-11ec-835d-0aa8762320bd'
+            ? 35000
+            : user?.organization === '4b3635cf-b937-11ed-83ab-02032078fba7'
+            ? 30000
+            : user?.organization === '0d3ea49a-c1e6-11ed-83ab-12e19e79322b'
+            ? 25000
+            : props.individualGoal,
       },
       reachedValue: {
         description: prevData.reachedValue.description,
@@ -89,7 +89,7 @@ function B2BRepresentativeArea(
         value: monthlyOrders?.monthlyOrdersDistinctClientAmount,
       },
     }))
-  }, [props, setData, monthlyOrders, user, goal])
+  }, [props, setData, monthlyOrders, user])
 
   const percent = useCallback(getPercentReachedValue, [data])
   const percentFormatted = useCallback(getPercentReachedValueFormatted, [data])
