63ff88b59c796252ddc7c1f32276070ddaf7f29b
Tiago de Andrade Freire <freire.tiago@gmail.com>
2023-05-07 23:06:42 -0300

    chore: debugging

--- react/components/B2BRepresentativeArea/B2BRepresentativeArea.tsx
+++ react/components/B2BRepresentativeArea/B2BRepresentativeArea.tsx
@@ -44,6 +44,9 @@ function B2BRepresentativeArea(
   const organization = data?.user?.organization
   const costCenter = data?.user?.costCenter
 
+  // eslint-disable-next-line no-console
+  console.log('B2B Context data on representative area:', data)
+
   useEffect(() => {
     if (organization) {
       // eslint-disable-next-line no-console
