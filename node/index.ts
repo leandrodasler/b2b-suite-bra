f767d4576d6749868bcf6a20733d017ca04a800e
Tiago de Andrade Freire <tiago.freire@cubos.io>
2023-07-31 20:17:53 -0300

    feat: clients config

--- node/index.ts
+++ node/index.ts
@@ -7,7 +7,7 @@ import getOrder from './middlewares/getOrder'
 import getOrders from './middlewares/getOrders'
 import setApiSettings from './middlewares/setApiSettings'
 
-const TIMEOUT_MS = 3 * 1000
+const TIMEOUT_MS = 4 * 1000
 const CONCURRENCY = 10
 const memoryCache = new LRUCache<string, never>({ max: 5000 })
 
@@ -17,8 +17,8 @@ const clients: ClientsConfig<Clients> = {
     default: {
       exponentialTimeoutCoefficient: 2,
       exponentialBackoffCoefficient: 2,
-      initialBackoffDelay: 50,
-      retries: 2,
+      initialBackoffDelay: 100,
+      retries: 10,
       timeout: TIMEOUT_MS,
       concurrency: CONCURRENCY,
     },
