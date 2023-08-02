14436e01e58848aba90b79e278fb9088d994100b
Tiago de Andrade Freire <tiago.freire@cubos.io>
2023-08-02 12:24:05 -0300

    feat: using sku specification "skuDisplaySequence" on sku-list

--- react/components/SkuList.tsx
+++ react/components/SkuList.tsx
@@ -11,10 +11,20 @@ enum Device {
   desktop = 'desktop',
 }
 
+const getSkuDisplaySequence = (item: Item) =>
+  +(
+    item.variations.find(variation => variation.name === 'skuDisplaySequence')
+      ?.values?.[0] ?? 0
+  )
+
+const compareItemsSequence = (a: Item, b: Item) =>
+  getSkuDisplaySequence(a) - getSkuDisplaySequence(b)
+
 const SkuListComponent = React.memo(({ device }: { device: Device }) => {
   const valuesFromContext = useContext(ProductContext)
   const items: Item[] = pathOr([], ['product', 'items'], valuesFromContext)
   const product: Product | undefined = path(['product'], valuesFromContext)
+  const sortedItems = [...items].sort(compareItemsSequence)
 
   const renderContent = (item: Item) => {
     switch (device) {
@@ -42,7 +52,7 @@ const SkuListComponent = React.memo(({ device }: { device: Device }) => {
 
   return (
     <div className="mw9 center pa3">
-      {items.map((item: Item) => renderContent(item))}
+      {sortedItems.map((item: Item) => renderContent(item))}
     </div>
   )
 })
