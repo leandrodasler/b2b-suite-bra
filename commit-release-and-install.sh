1fc007619eeee36ff53c038d61f407c6dbdf11a7
Tiago de Andrade Freire <tiago.freire@cubos.io>
2023-08-10 16:21:37 -0300

    chore: commit, release and install shell script

---
+++ commit-release-and-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+REPO=$(git remote get-url --push origin)
+echo $'\n=============== Commiting changes to repository '$REPO
+echo "Enter commit description: "
+read description
+git add .
+git commit -m "$description"
+git push origin main
+
+echo $'\n=============== Releasing app'
+vtex switch ssesandbox04
+vtex release patch stable
+vtex deploy --force --yes
+
+echo $'\n=============== Intalling app on B2B Stores'
+vtex switch bravtexfashionb2b
+vtex use master
+vtex install
+vtex switch vtextitantools
+vtex use master
+vtex install
+vtex switch vtexfashionb2b
+vtex use master
+vtex install
